d877ea722f24a346d0e8a5775b62436b8717d243
nowgnas <marshmello.sw@gmail.com>
2022-04-24 01:09:35 +0900

    add search console

--- src/components/layout.js
+++ src/components/layout.js
@@ -21,15 +21,20 @@ const Layout = ({ location, title, children }) => {
   }
 
   return (
-    <div className="global-wrapper" data-is-root-path={isRootPath}>
-      <header className="global-header">{header}</header>
-      <main>{children}</main>
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
-      </footer>
-    </div>
+    <>
+      <div className="body-wrapper">
+        <div>hello</div>
+        <div className="global-wrapper" data-is-root-path={isRootPath}>
+          <header className="global-header">{header}</header>
+          <main>{children}</main>
+          <footer>
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <a href="https://www.gatsbyjs.com">Gatsby</a>
+          </footer>
+        </div>
+      </div>
+    </>
   )
 }
 
