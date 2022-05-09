94594f01e74f30bf27bef846944c233b0bbb4867
nowgnas <marshmello.sw@gmail.com>
2022-05-09 14:44:58 +0900

    sidebar 나누기

---
+++ src/components/sidebar.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+const Sidebar = () => {
+    return (
+        <>
+            <div className="sidebar">
+                <div>blog title</div>
+                <div>author </div>
+            </div>
+        </>
+    )
+}
+export default Sidebar
