64867ea229ba4018651dea3a15147da191e38cfd
nowgnas <marshmello.sw@gmail.com>
2022-05-09 15:10:36 +0900

    error발생

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,11 +1,43 @@
 import * as React from "react"
+import { StaticImage } from "gatsby-plugin-image"
+import { useStaticQuery, graphql, Link } from "gatsby"
 
 const Sidebar = () => {
+    const data = useStaticQuery(graphql`
+        query BioQuery {
+            site {
+                siteMetadata {
+                    author {
+                        name
+                        summary
+                    }
+                    social {
+                        twitter
+                    }
+                }
+            }
+        }
+    `)
+    const author = data.site.siteMetadata?.author
+    const social = data.site.siteMetadata?.social
+
     return (
         <>
             <div className="sidebar">
-                <div>blog title</div>
-                <div>author </div>
+                <Link to="/">
+                    {/* <img src="../images/pingus.png" /> */}
+                    <StaticImage
+                        className="bio-avatar"
+                        formats={["auto", "webp", "avif"]}
+                        src="../images/pingus.png"
+                        width={50}
+                        height={50}
+                        quality={95}
+                        alt="Profile picture"
+                    />
+                </Link>
+                <div className="sidebar-title">blog title</div>
+                <div>{author} </div>
             </div>
         </>
     )
